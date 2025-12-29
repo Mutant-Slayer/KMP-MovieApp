add98b8f8fea34e200884d326dfc5a8eb182344e
Shikoh <anas.shikoh@kukufm.com>
2025-12-29 23:39:39 +0530

    response class change

--- composeApp/src/commonMain/kotlin/org/example/kmp/movieapp/domain/MovieDetails.kt
+++ composeApp/src/commonMain/kotlin/org/example/kmp/movieapp/domain/MovieDetails.kt
@@ -5,52 +5,35 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class MovieDetails(
-    @SerialName("Title")
-    val title: String,
-    @SerialName("Year")
-    val year: String,
-    @SerialName("Rated")
-    val rated: String,
-    @SerialName("Released")
-    val released: String,
-    @SerialName("Runtime")
-    val runtime: String,
-    @SerialName("Genre")
-    val genre: String,
-    @SerialName("Director")
-    val director: String,
-    @SerialName("Writer")
-    val writer: String,
-    @SerialName("Actors")
-    val actors: String,
-    @SerialName("Plot")
-    val plot: String,
-    @SerialName("Language")
-    val language: String,
-    @SerialName("Country")
-    val country: String,
-    @SerialName("Awards")
-    val awards: String,
-    @SerialName("Poster")
-    val poster: String,
-    @SerialName("Ratings")
-    val ratings: List<Rating>,
-    @SerialName("Metascore")
-    val metascore: String,
-    val imdbRating: String,
-    val imdbVotes: String,
-    @SerialName("imdbID")
+    val adult: Boolean,
+    @SerialName("backdrop_path")
+    val backdropPath: String? = null,
+    val budget: Int,
+    val genres: List<Genre>,
+    val homepage: String,
+    val id: Int,
+    @SerialName("imdb_id")
     val imdbId: String,
-    @SerialName("Type")
-    val type: String,
-    @SerialName("DVD")
-    val dvd: String,
-    @SerialName("BoxOffice")
-    val boxOffice: String,
-    @SerialName("Production")
-    val production: String,
-    @SerialName("Website")
-    val website: String,
-    @SerialName("Response")
-    val response: String,
+    @SerialName("origin_country")
+    val originCountry: List<String>,
+    @SerialName("original_language")
+    val originalLanguage: String,
+    @SerialName("original_title")
+    val originalTitle: String,
+    val overview: String,
+    val popularity: Double,
+    @SerialName("poster_path")
+    val posterPath: String? = null,
+    @SerialName("release_date")
+    val releaseDate: String,
+    val revenue: Int,
+    val runtime: Int,
+    val status: String,
+    val tagline: String,
+    val title: String,
+    val video: Boolean,
+    @SerialName("vote_average")
+    val voteAverage: Double,
+    @SerialName("vote_count")
+    val voteCount: Int
 )
